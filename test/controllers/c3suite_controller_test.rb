cae5ad255301a62e9666f6b8293f6e37a5a08d31
naikhpratik <pratik.naik@mavs.uta.edu>
2016-06-28 00:26:55 -0500

    Clean

--- test/controllers/c3suite_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class C3suiteControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get c3suite_index_url
-    assert_response :success
-  end
-
-end
