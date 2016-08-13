83844f32caecdcb495c16e9d2e651eef87001e7d
naikhpratik <pratik.naik@mavs.uta.edu>
2016-08-12 21:25:08 -0500

    Devise functioning

--- app/controllers/pusers/sessions_controller.rb
+++ app/controllers/pusers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Pusers::SessionsController < Devise::SessionsController
-# before_action :configure_sign_in_params, only: [:create]
+ before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -7,8 +7,8 @@ class Pusers::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
+   #def create
+  #
   # end
 
   # DELETE /resource/sign_out
--- config/routes.rb
+++ config/routes.rb
@@ -7,31 +7,36 @@ Rails.application.routes.draw do
 
   devise_for :pusers, :controllers => { registrations: 'pusers/registrations' }
   #devise_for :pusers, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout"}
+
   #get 'admin/index'
   #get 'sessions/new'
   #get 'sessions/create'
   #get 'sessions/destroy'
   #get 'users/userpage'
-  #resources :users
-  #resources :admin
+  resources :users
+  resources :admin
   #get 'api/index'
-  #get '/product'=>'users#product'
+  get '/product'=>'users#product'
  #get '/users' => 'users#index'
- #get '/userpage' => 'users#userpage'
+ get '/userpage' => 'users#userpage'
   #get 'sessions' => 'sessions#new'
   #get 'users' => 'users#new'
   #get 'search' => 'search#index'
   #get 'logout' => 'sessions#destroy'
   #get 'users' => 'users#userpage'
-#  get 'admin' => 'admin#index'
-  #controller :sessions do
+  get 'admin' => 'admin#index'
+  controller :sessions do
   #  get  'login' => :new
-  #  post 'login' => :create
-  #  post 'Make admin' => :makeadmin
-  #end
+    #post 'login' => :create
+    #post 'Make admin' => :makeadmin
+  end
+
+  controller :users do
+    get 'Make admin' => :makeadmin
+  end
 
 root 'page#home'
-#match ':controller(/:action(/:id))', :via => :get
+match ':controller(/:action(/:id))', :via => :get
   #get "sessions/create"
   #get "sessions/destroy"
 
