56c4109040413b46842f265fc7c2f38104742aa4
= <=>
2016-09-21 17:55:25 +0500

    Add the ability to manipulate user and their roles through privileged accounts. Some code cleanup.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,19 +1,57 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
   	@users = User.all_except(current_user)
   end
 
-  def make_admin
-  	u = User.find(params[:user_id])
-  	u.isadmin = u.isadmin ? false : true
+  def show
+  end
 
-  	if u.save
-  		flash[:notice] = "Successfully updated the User record."
-  	else
-  		flash[:error] = "There was some problem updating the User record. Please try again."
-  	end
+  def new
+    @user = User.new
+  end
 
-  	redirect_to admin_users_path
+  def edit
+    @user_roles = @user.roles.map(&:name)
+  end
+
+  def create
+    @user = User.new(user_params)
+    user_roles = []
+
+    user_params[:role_ids].each do |role_id|
+      user_roles.push(UserRole.create({user_id: @user.id, role_id: role_id})) unless role_id.blank?
+    end
+
+    @user.user_roles = user_roles
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to admin_user_url(@user), notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    @user.user_roles.destroy
+
+    respond_to do |format|
+      if (password_upchanged? && @user.update_without_password(user_params)) || @user.update(user_params)
+        format.html { redirect_to admin_user_url(@user), notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_url, notice: 'User was successfully destroyed.' }
+    end
   end
 
   private
@@ -24,6 +62,27 @@ class Admin::UsersController < Admin::BaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation).tap do |whitelisted|
+        whitelisted[:role_ids] = get_new_ids params[:role_ids]
+      end
+    end
+
+    def password_upchanged?
+      user_params[:password].blank?
+    end
+
+    # TODO: Find a better alternative
+    def get_new_ids role_names
+      roles = Role.all.select(:name, :id)
+      role_names = role_names
+      role_ids = []
+
+      role_names.each do |role_name|
+        roles.each do |role|
+          role_ids.push(role.id) if role.name == role_name
+        end
+      end
+
+      role_ids
     end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ApplicationRecord
-  validates_presence_of :name
+  validates :name, uniqueness: true, presence: true
   
   has_many :user_roles
   has_many :users, :through => :user_roles
