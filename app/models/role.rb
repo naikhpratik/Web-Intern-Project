05b4170f030286600e8f30e6bde869989311eb66
= <=>
2016-09-23 01:34:01 +0500

    Added Admin's ability to associate users with products roles.

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,8 @@ class Role < ApplicationRecord
   
   has_many :user_roles
   has_many :users, :through => :user_roles
+
+  def self.all_except ids
+    where.not(id: ids).map(&:id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922092514) do
+ActiveRecord::Schema.define(version: 20160922180837) do
 
   create_table "contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "product_id"
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 20160922092514) do
   end
 
   create_table "user_products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "relationship"
     t.integer  "product_id"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "role_id"
     t.index ["product_id"], name: "index_user_products_on_product_id", using: :btree
     t.index ["user_id"], name: "index_user_products_on_user_id", using: :btree
   end
