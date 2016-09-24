21d863af99fcd4670cf9b495e757c840cbc8ce8b
= <=>
2016-09-24 18:50:55 +0500

    Updated tests to replicate the code changes.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
 
   def index
       @products = Product.where(visibility: ["All", "Registered Users"])
-      @my_products = current_user.products || []
+      @my_products = current_user.products || [] if current_user
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  it "is valid with a name" do
+RSpec.describe Product, type: :model do  
+  it "is valid with valid attributes" do
     product = FactoryGirl.create(:product)
     expect(product).to be_valid
   end
 
   it "is not valid without a name" do
-  	product = Product.new
+    product = Product.new(visibility: "All")
+    expect(product).to be_invalid
+  end
+
+  it "is not valid without visibility" do
+    product = Product.new(name: "Test Product 1")
   	expect(product).to be_invalid
   end
 end
