480573f5a5ffa619c8e9747e87010b59538cec61
= <=>
2016-09-25 13:17:53 +0500

    (not to this branch) Added username field for sign up and updated user spec.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
  #before_action :configure_sign_up_params, only: [:create]
+ before_action :configure_sign_up_params, only: [:create]
  before_action :configure_account_update_params, only: [:update]
  before_action :configure_account_update_params, if: :devise_controller?
   # GET /resource/sign_up
@@ -39,10 +40,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   #def configure_sign_up_params
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-     #params.require(:user).permit(:email, :password, :password_confirmation)
-   #end
+   def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, :password_confirmation])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
