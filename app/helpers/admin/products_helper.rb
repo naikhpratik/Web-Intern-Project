6805673fc247b40b6543c6fd8d66052dbcce0340
naikhpratik <pratik.naik@mavs.uta.edu>
2016-09-26 17:05:08 -0500

    module

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,4 +1,5 @@
 module Admin::ProductsHelper
+
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
@@ -8,4 +9,5 @@ module Admin::ProductsHelper
     end
     link_to(name, '#', class:  "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
+
 end
