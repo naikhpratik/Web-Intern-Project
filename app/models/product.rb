c3bf6b8c14140765c97e21778a5efeec5df0a4bd
naikhpratik <pratik.naik@mavs.uta.edu>
2016-09-26 13:23:17 -0500

    Add module part 1

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   has_many :user_products
   has_many :contents
   has_many :users, :through => :user_products
+  accepts_nested_attributes_for :contents, allow_destroy: true
 end
