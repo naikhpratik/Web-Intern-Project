70dbb420b54115ecfbd5f3e9c80a934a753ca176
= <=>
2016-10-06 13:56:24 +0500

    Tests for new models.

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
+require "active_record/acts_as/matchers"
 
 RSpec.describe Content, type: :model do
+  it { expect(Content).to be_actable }
 
   it { should belong_to(:product) }
 
   it "is valid with valid attributes" do
-    content = Content.new
+    content = FactoryGirl.create(:content)
     expect(content).to be_valid
   end
 
