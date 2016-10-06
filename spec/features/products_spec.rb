1c65c54c95a37bf50710ed44b311ee1605016732
naikhpratik <pratik.naik@mavs.uta.edu>
2016-10-06 11:50:26 -0500

    Change small

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -3,22 +3,23 @@ require 'capybara/rspec'
 
 RSpec.feature "Product", :type => :feature do
   scenario "User creates a valid product" do
-    login    
+    login
     visit new_admin_product_url
 
     fill_in 'Name', :with => "My Widget"
     select "All", from: 'product[visibility]'
     click_button "Create"
 
+    
     expect(page).to have_text("Product was successfully created")
   end
 
   scenario "User creates a duplicate product" do
     product = FactoryGirl.create(:product, name: "My Widget")
-    
+
     login
     visit new_admin_product_url
-    
+
     fill_in "Name", :with => "My Widget"
     select "All", from: 'product[visibility]'
     click_button "Create"
