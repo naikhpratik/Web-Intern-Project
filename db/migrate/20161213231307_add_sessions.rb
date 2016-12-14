b04c5b9da1b01381c616e8654c17d6cd2cd92cb4
naikhpratik <pratik.naik@mavs.uta.edu>
2016-12-14 00:36:39 -0600

    Fixed

--- db/migrate/20161213231307_add_sessions.rb
+++ db/migrate/20161213231307_add_sessions.rb
@@ -1,7 +1,7 @@
 class AddSessions < ActiveRecord::Migration[5.0]
   def change
     create_table :sessions do |t|
-      t.text :session_id,  null: false, :length=>1000
+      t.text :session_id,  null: false
       t.longtext :data
 
       ## Rememberable
@@ -9,6 +9,6 @@ class AddSessions < ActiveRecord::Migration[5.0]
       t.datetime :updated_at
 
     end
-    add_index :sessions, :session_id,    unique: true, :length=>1000
+    add_index :sessions, :session_id,    unique: true
   end
 end
