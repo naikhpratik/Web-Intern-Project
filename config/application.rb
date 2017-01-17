1300bb190b508cc96bc2624706f1c8985872b3fb
Umar Ali <aliumar.me@gmail.com>
2017-01-17 18:35:22 +0500

    Another try to fix ckeditor config issue.

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,9 @@ module C3Suite
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     #nope#
     config.assets.precompile += ['jquery.countdown.js', 'jquery.countdown.css','jquery.plugin.js']
   end
