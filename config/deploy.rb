94e3be942e412c332e7a8b63eb24557ab150d63b
Umar Ali <aliumar.me@gmail.com>
2017-01-17 19:06:52 +0500

    None worked, another try with code deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,9 @@ set :rvm_path, '~/.rvm'
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.1'
 
+# set the locations that we will look for changed assets to determine whether to precompile
+set :assets_dependencies, %w(app/assets lib/assets vendor/assets public/assets/ckeditor Gemfile.lock config/routes.rb)
+
 namespace :deploy do
 
 
@@ -80,6 +83,43 @@ namespace :deploy do
       end
     end
   end
+
+  namespace :assets do
+    desc "Precompile assets"
+    task :precompile do
+      on roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            begin
+              # find the most recent release
+              latest_release = capture(:ls, '-xr', releases_path).split[1]
+
+              # precompile if this is the first deploy
+              raise PrecompileRequired unless latest_release
+
+              latest_release_path = releases_path.join(latest_release)
+
+              # precompile if the previous deploy failed to finish precompiling
+              execute(:ls, latest_release_path.join('assets_manifest_backup')) rescue raise(PrecompileRequired)
+
+              fetch(:assets_dependencies).each do |dep|
+                # execute raises if there is a diff
+                execute(:diff, '-Naur', release_path.join(dep), latest_release_path.join(dep)) rescue raise(PrecompileRequired)
+              end
+
+              info("Skipping asset precompile, no asset diff found")
+
+              # copy over all of the assets from the last release
+              execute(:cp, '-r', latest_release_path.join('public', fetch(:assets_prefix)), release_path.join('public', fetch(:assets_prefix)))
+            rescue PrecompileRequired
+              execute(:rake, "assets:precompile") 
+            end
+          end
+        end
+      end
+    end
+  end
+
 
   after :publishing, :stopstart
   before :check, :setup_config
