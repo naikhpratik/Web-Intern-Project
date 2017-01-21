a5fc0c8e8de15d55b7ba701e07a2f2938c8af24d
Daniel Zacek <daniel@zacek.com>
2017-01-21 14:32:22 -0700

    fix bugherd framing

--- app/assets/javascripts/bugherd.js
+++ app/assets/javascripts/bugherd.js
@@ -1,8 +1,6 @@
-<script type='text/javascript'>
  (function (d, t) {
    var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
    bh.type = 'text/javascript';
    bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=v8gwlv0thvefnfomxditjg';
    s.parentNode.insertBefore(bh, s);
    })(document, 'script');
- </script>
