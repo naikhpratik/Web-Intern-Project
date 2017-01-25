c3b35e3a4ae8acd783dbce3622c88c67d60552ac
Umar Ali <aliumar.me@gmail.com>
2017-01-25 11:48:25 +0500

    Remove sanitization to allow all html tags and attributes

--- app/models/html.rb
+++ app/models/html.rb
@@ -3,8 +3,8 @@ require 'sanitize'
 class Html < ApplicationRecord
   acts_as :content
 
-  def html_source=(html)
-    sanitized = Sanitize.document(html, Sanitize::Config::RELAXED)
-    write_attribute(:html_source, sanitized)
-  end
+  # def html_source=(html)
+  #   sanitized = Sanitize.document(html, :allow_doctype => true)
+  #   write_attribute(:html_source, sanitized)
+  # end
 end
