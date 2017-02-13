19e85b6d09162984badae8fb1fc57e7d1323a811
Daniel Zacek <daniel@zacek.com>
2017-02-12 20:20:44 -0700

    detailed product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show]
+  layout 'product'
 
   def index
     @products = user_signed_in? ? current_user.products : []
   end
 
   def show
+
   end
 
   private
