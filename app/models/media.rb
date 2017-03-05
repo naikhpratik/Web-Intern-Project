3ee8cf28fa895c7d67c65e525cc26a500ca98980
Umar Ali <aliumar.me@gmail.com>
2017-03-06 00:46:42 +0500

    Switch storage to S3. Add proper validations. Misc. changes.

--- app/models/media.rb
+++ app/models/media.rb
@@ -4,12 +4,12 @@ class Media < ApplicationRecord
   mount_uploader :source, MediaUploader
   process_in_background :source
 
-  MEDIA_TYPES = ['Audio', 'Video']
+  MEDIA_TYPES = ['Image', 'Audio', 'Video']
 
   validate :is_valid_media_local_type?
   validates_presence_of :caption
 
   def is_valid_media_local_type?
-    errors.add(:local_type, "must be Audio or Video") unless MEDIA_TYPES.include?(local_type.capitalize)
+    errors.add(:local_type, "must be #{MEDIA_TYPES.join(' or ')}") unless MEDIA_TYPES.include?(local_type.capitalize)
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
-# require 'fog/aws'
+require 'fog/aws'
 
-# CarrierWave.configure do |config|
-#   # config.fog_provider = 'fog/aws'
-#   # config.fog_credentials = {
-#   #   provider: 'AWS',
-#   #   aws_access_key_id: ENV['S3_KEY'],
-#   #   aws_secret_access_key: ENV['S3_SECRET']
-#   # }
-#   # config.fog_directory  = 'code3suite'
-#   # config.fog_public     = false
-# end
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['S3_KEY'],
+    aws_secret_access_key: ENV['S3_SECRET']
+  }
+  config.fog_directory  = 'code3suite'
+  config.fog_public     = false
+end
