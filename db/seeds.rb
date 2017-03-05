9dc626f189f672b8fb9bcc29af9738dbcb5d8f05
Umar Ali <aliumar.me@gmail.com>
2017-03-06 00:58:48 +0500

    Remove conflict in log file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'aadmin', password: 'user1234', email: 'aadmin@example.com', roles: [Role.find(1)])
+User.create(username: 'PM1', password: 'user1234', email: 'pm1@example.com', roles: [Role.find(2)])
+User.create(username: 'PM2', password: 'user1234', email: 'pm2@example.com', roles: [Role.find(2)])
+User.create(username: 'CM1', password: 'user1234', email: 'cm1@example.com', roles: [Role.find(3)])
+User.create(username: 'CM1', password: 'user1234', email: 'cm1@example.com', roles: [Role.find(3)])
+User.create(username: 'CC2', password: 'user1234', email: 'cc2@example.com', roles: [Role.find(4)])
+User.create(username: 'I1', password: 'user1234', email: 'i1@example.com', roles: [Role.find(5)])
+User.create(username: 'I2', password: 'user1234', email: 'i1@example.com', roles: [Role.find(5)])
