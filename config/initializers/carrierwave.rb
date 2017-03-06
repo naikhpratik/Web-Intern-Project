ffac982a75782bf0725e353cefb3243ade0b284e
Umar Ali <aliumar.me@gmail.com>
2017-03-06 17:52:47 +0500

    Update key names to fix the missing key errors.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET']
+    aws_access_key_id: ENV['aws_access_key_id'],
+    aws_secret_access_key: ENV['aws_secret_access_key']
   }
   config.fog_directory  = 'code3suite'
   config.fog_public     = false
