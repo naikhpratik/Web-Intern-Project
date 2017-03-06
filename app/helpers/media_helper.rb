67bbc707a2156b9d6cec1093cd1404a3d974ae73
Umar Ali <aliumar.me@gmail.com>
2017-03-06 23:27:45 +0500

    Handle nil case for media_helper method.

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -1,11 +1,12 @@
 module MediaHelper
   def default_source_tmp_path media
+    return "" if media.blank?
     if media.source_url.blank? && media.source_tmp.present?
       "/uploads/tmp/" << media.source_tmp 
     elsif media.source_url.present?
       media.source_url
     else
-      nil
+      ""
     end
   end
 end
